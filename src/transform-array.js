e1ed8ad74dcf631d7cafbedb74f5470b11b77288
Yakubovich-siarhei <yakubovich.siarhei@mail.ru>
2023-04-15 10:33:32 +0300

    feat: array (April 15, 2023 17:30:03 PM)

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  let result = [];
+  let items = ['--discard-next', '--discard-prev', '--double-next', '--double-prev']
+  for(let i = 0; i < arr.length; i++){
+      if(!items.includes(arr[i])) {
+        result.push(arr[i])
+      }
+      switch(arr[i]) {
+        case '--discard-next':
+          if((i + 1) < arr.length && !items.includes(arr[i + 1])){
+            i = i + 1;
+          }
+          break;
+        case '--discard-prev':
+          if(result.length !== 0 && arr[i - 1] == result[result.length - 1] && !items.includes(arr[i - 1])){
+            result.pop();
+          }
+          break;
+        case '--double-next':
+          if((i + 1) < arr.length && !items.includes(arr[i + 1])){
+            result.push(arr[i + 1]);
+          }
+          break;
+        case '--double-prev':
+          if((i - 1) >= 0 && !items.includes(arr[i - 1]) && arr[i - 1] === result[result.length - 1]) {
+            result.push(arr[i - 1]);
+          }
+          break;
+      }
+  }
+
+  return result;
 }
 
 module.exports = {
